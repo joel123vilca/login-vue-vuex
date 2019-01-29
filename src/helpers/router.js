8636213e5f7d23cce1422590a64ef8ccd2565e79
joel123vilca <joevil23tjq@gmail.com>
2019-01-29 01:48:17 -0500

    probando en el localstorage

---
+++ src/helpers/router.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import HomePage from '../components/Home.vue'
+import LoginPage from '../components/Login.vue'
+import RegisterPage from '../components/Register.vue'
+
+Vue.use(Router);
+
+export const router = new Router({
+  mode: 'history',
+  routes: [
+    { path: '/', component: HomePage },
+    { path: '/login', component: LoginPage },
+    { path: '/register', component: RegisterPage },
+
+    // otherwise redirect to home
+    { path: '*', redirect: '/' }
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  next();
+})
